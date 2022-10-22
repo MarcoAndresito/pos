5114e8eec6feb204cc9072460ea45d0b6bafb5e0
Andres Vasquez <you@example.com>
2022-10-21 22:44:23 -0400

    model - migration - factory - seed

--- routes/web.php
+++ routes/web.php
@@ -31,18 +31,12 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
 });
-
-
-
-
-
-
-
-
 
 Route::get('/saludar', function () {
     return '!Hola Munddo¡';
