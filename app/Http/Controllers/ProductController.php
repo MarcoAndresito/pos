201afcf08dbda93061b535ecde32b185ebe154f6
Andres Vasquez <you@example.com>
2022-11-09 22:29:59 -0400

    add model migration factory seed contgrolller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->has('q')) {
+            $products = Product::all()->where('name', 'like', "%$request->q%");
+        } else {
+            $products = Product::all();
+        }
+        return Inertia::render('Category/Index', [
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Product/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Product::create($request->validate([
+            'user_id' => 'required',
+            'category_id' => 'required',
+            'name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+        ]));
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return Inertia::render('Product/show', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return Inertia::render('Product/Edit', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->validate([
+            'user_id' => 'required',
+            'category_id' => 'required',
+            'name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+        ]));
+        return redirect()->route('category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('product.index');
+    }
+}
